568bb420f51f6e2000e80c19a6b5a758d403c93f
Kushal Parekh <kup9@pitt.edu>
2025-03-27 19:24:39 -0400

    need to test copy script

--- copy_script.py
+++ copy_script.py
@@ -7,7 +7,21 @@ repo_path = "/root/ImageDatasetMapping"
 
 try:
     shutil.copytree(images_color_path, os.path.join(repo_path, "images_Color"))
+    shutil.copytree(images_depth_path, os.path.join(repo_path, "images_Depth"))
 except:
-    
+    for f in os.listdir(images_color_path): 
+        if os.path.isdir(os.path.join(images_color_path, f)):
+            try:
+                shutil.copytree(os.path.join(images_color_path, f), os.path.join(repo_path, f))
+                shutil.rmtree(os.path.join(images_color_path, f))
+            except:
+                print(f"{f} folder already exists")
 
 
+    for f in os.listdir(images_depth_path):
+        if os.path.isdir(os.path.join(images_depth_path, f)):
+            try:
+                shutil.copytree(os.path.join(images_depth_path, f), os.path.join(repo_path, f))
+                shutil.rmtree(os.path.join(images_depth_path, f))
+            except:
+                print(f"{f} folder already exists")
